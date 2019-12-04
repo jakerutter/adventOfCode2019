f18466c57981d64eb6a839f6262e637200cd405e
Jacob Rutter <rutter.jacob@gmail.com>
2019-12-04 14:42:07 -0500

    Advent of Code 2019. day 4.
    The day4.js script is meant to run with node. Type "node day4.js" into the terminal in the directory with this script to execute. There is a timer that displays how long the script takes to execute.

---
+++ day4.js
@@ -0,0 +1,65 @@
+
+function getPasswordCount(considerOverMatch){
+    var passwordCount = 0;
+    var range = [156218, 652527];
+    var possiblePasswords = [];
+
+    for(let i=156218; i<=652527; i++){
+
+        let split = ('' + i).split('');
+        let slippage = false;
+        let matchCount = 0;
+        let multMatchNum = [];
+        let singleMatch = [];
+
+        for (let k=0; k<split.length; k++){
+
+            slippage = split[k] > split[k+1] ? true : false;
+            if (slippage){ break; }
+
+            split[k] === split[k+1] ? matchCount += 1 : "";
+            singlematch = split[k] === split[k+1] ? singleMatch.push(split[k]) : singleMatch;
+
+            if(considerOverMatch){
+
+                if (split[k] === split[k+1] && split[k+1] === split[k+2])
+                { 
+                    multMatchNum.push(split[k]); 
+                }
+            }
+        }
+
+        if (considerOverMatch && multMatchNum.length > 0 && singleMatch.length > 0){
+            for(let z=0; z<multMatchNum.length; z++){
+                for(let y=0; y<singleMatch.length; y++){
+                    if(multMatchNum[z] === singleMatch[y]){
+                        var filtered = singleMatch.filter(function(element){
+                            return element !== singleMatch[y];
+                        });
+                        singleMatch = filtered;
+                    }
+                }
+            }
+        }
+
+        if (!slippage && !considerOverMatch && matchCount > 0 || !slippage && considerOverMatch && singleMatch.length > 0){
+            possiblePasswords.push(i);
+        }
+    }
+    passwordCount = possiblePasswords.length;
+    return passwordCount;
+}
+
+
+function run() {
+    console.time('day4');
+
+    let count = getPasswordCount(false);
+    console.log('the number of password possibilities: ' + count);
+
+    let secondCount = getPasswordCount(true);
+    console.log('the number of password possibilities with the new rules: ' + secondCount);
+    console.timeEnd('day4');
+}
+
+run();
